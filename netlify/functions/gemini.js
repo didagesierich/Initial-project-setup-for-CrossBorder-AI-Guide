4f89a6b9ae02003fbe66b6f0f4751577b2b330e1
didagesierich <cobzarudida@ymail.com>
2026-03-15 13:19:30 +0100

    Create gemini.js

---
+++ netlify/functions/gemini.js
@@ -0,0 +1,24 @@
+export async function handler(event) {
+
+  const API_KEY = process.env.GEMINI_API_KEY;
+
+  const body = JSON.parse(event.body);
+
+  const response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    }
+  );
+
+  const data = await response.json();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  };
+}
